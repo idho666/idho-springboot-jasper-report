52f7fb4ca36f007be44428922603f1c22d252af0
hafidh bm <hafidh.mauidho@iconpln.co.id>
2024-08-16 21:04:18 +0700

    init project

---
+++ jasperreport/src/test/java/com/idhodev/jasperreport/JasperreportApplicationTests.java
@@ -0,0 +1,13 @@
+package com.idhodev.jasperreport;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JasperreportApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
